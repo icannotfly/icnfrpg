ff64f693fe467b94b8bb0dd7efeba07acffaae9b
icannotfly <icannotfly@icannotfly.net>
2020-10-26 01:44:53 -0700

    add WASD movement
    closes #1

---
+++ icnfrpg/Private/icnfrpgCharacter.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "icnfrpgCharacter.h"
+
+// Sets default values
+AicnfrpgCharacter::AicnfrpgCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AicnfrpgCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AicnfrpgCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector() * Value);
+}
+
+void AicnfrpgCharacter::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector() * Value);
+}
+
+// Called every frame
+void AicnfrpgCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AicnfrpgCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AicnfrpgCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AicnfrpgCharacter::MoveRight);
+}
+
---
+++ icnfrpg/Public/icnfrpgCharacter.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "icnfrpgCharacter.generated.h"
+
+UCLASS()
+class ICNFRPG_API AicnfrpgCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AicnfrpgCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Move the character forward (value = 1) or backward (value = -1)
+	void MoveForward(float Value);
+
+	// Move the character right (value = 1) or left (value = -1)
+	void MoveRight(float Value);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
